7be998b94ae609e5c54025e0d2f52d0d2c203a0f
Paul K. Korir, PhD <pkorir@ebi.ac.uk>
2021-02-01 13:01:35 +0000

    broken test

--- new_main/tests/test_sta.py
+++ new_main/tests/test_sta.py
@@ -15,3 +15,11 @@ class TestCLI(unittest.TestCase):
         print(args)
         self.assertEqual(args.table, f"{file_root}.em")
         self.assertEqual(args.average, f"{file_root}.map")
+
+    def test_explicit(self):
+        """User explicitly specifies files"""
+        file_root = f"{os.path.join(TEST_DATA, 'motl')}/file"
+        sys.argv = shlex.split(f"cmd -T {file_root}.em -A {file_root}.map")
+        args = parse_args()
+        print(args)
+
