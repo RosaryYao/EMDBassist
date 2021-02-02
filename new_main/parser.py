c6286bdbf312748722b41323849a4ce0881008b8
RosaryYao <rosaryyao@outlook.com>
2021-02-02 10:57:02 +0000

    fix path for windows

--- new_main/parser.py
+++ new_main/parser.py
@@ -1,16 +1,18 @@
 import argparse
 import os
 import sys
+import pathlib
+import platform
 
 parser = argparse.ArgumentParser(prog='cmd', description='Generic structure of sta')
-parser.add_argument('file')
+parser.add_argument('file', nargs="?")
 parser.add_argument('-T', '--table', help='table of coordinates')
 parser.add_argument('-A', '--average', help='averaged density')
 
 
 def _check_file_types(args):
     """returns indicator of which file type"""
-    if os.path.exists(f"{args.file}.em") and os.path.exists(f"{args.file}.map"):  # Briggs
+    if os.path.exists(f"{args.file}.em") and pathlib.Path(f"{args.file}.map").exists():  # Briggs
         return 0
     elif os.path.exists(f"./{args.file}.tbl") and os.path.exists(f"./{args.file}.em"):  # Dynamo
         return 1
@@ -23,8 +25,13 @@ def _check_file_types(args):
 def parse_args():
     args = parser.parse_args()
     if args.file:
+        if platform.system() == "Windows":
+            args.file = args.file.replace(os.sep, "\\")
+        else:
+            args.file = args.file.reaplce(os.sep, "/")
         # check for the presence of different file type s
         file_type = _check_file_types(args)
+        print(f"file type: {file_type}")
         # assuming Brigg's data
         if file_type == 0:
             args.table = f"{args.file}.em"
@@ -40,5 +47,5 @@ def parse_args():
             print(f"please ensure that {args.file}.<table extension> and {args.file}.<average extension> exist",
                   file=sys.stderr)
             print(f"alternatively, use -T/--table <file> -A/--average <file> to run", file=sys.stderr)
-            sys.exit(os.EX_USAGE)
+            sys.exit(0)
     return args
--- new_main/tests/test_sta.py
+++ new_main/tests/test_sta.py
@@ -2,6 +2,8 @@ import shlex
 import sys
 import unittest
 import os
+import pathlib
+import platform
 
 from .. import TEST_DATA
 from ..parser import parse_args
@@ -10,9 +12,18 @@ from ..parser import parse_args
 class TestCLI(unittest.TestCase):
     def test_default(self):
         file_root = f"{os.path.join(TEST_DATA, 'motl')}/file"
-        sys.argv = shlex.split(f"cmd {file_root}")
+        print(file_root)
+        if platform.system() == "Windows":
+            file_root = os.path.normcase(file_root)
+        print(f"value of file_root: {file_root}")
+        # sys.argv = shlex.split(f"cmd {file_root}")
+        sys.argv = f"cmd {file_root}".split(" ")
+        print(sys.argv)
         args = parse_args()
-        print(args)
+        print(f"The args.file is: {args.file}")
+        print(os.path.exists(f"{args.file}.em"))
+        print(os.path.exists(f"{file_root}.em"))
+        self.assertEqual(args.file, file_root)
         self.assertEqual(args.table, f"{file_root}.em")
         self.assertEqual(args.average, f"{file_root}.map")
 
@@ -20,6 +31,7 @@ class TestCLI(unittest.TestCase):
         """User explicitly specifies files"""
         file_root = f"{os.path.join(TEST_DATA, 'motl')}/file"
         sys.argv = shlex.split(f"cmd -T {file_root}.em -A {file_root}.map")
+        print(sys.argv)
         args = parse_args()
         print(args)
 
